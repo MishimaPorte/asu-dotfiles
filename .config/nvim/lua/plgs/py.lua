447f34fcf5745c5c244acd65cc2c5c6609b40e0b
Арсений Байгузин <porte@apsolutions.ru>
2023-06-09 13:02:54 +0300

    configs

---
+++ .config/nvim/lua/plgs/py.lua
@@ -0,0 +1,20 @@
+null_ls = require("null-ls")
+
+
+local sources = {
+  -- python
+  null_ls.builtins.formatting.black.with({
+    extra_args = { "--line-length=120" }
+  }),
+  null_ls.builtins.formatting.isort,
+}
+
+null_ls.setup({ sources = sources })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.py",
+  callback = function()
+      vim.lsp.buf.format()
+  end,
+  group = format_sync_grp,
+})
