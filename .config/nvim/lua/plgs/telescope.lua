a4b9179df7e316015ba9fbe4d77b1f9991d752ba
Арсений Байгузин <porte@apsolutions.ru>
2023-06-26 13:29:07 +0300

    a

---
+++ .config/nvim/lua/plgs/telescope.lua
@@ -0,0 +1,27 @@
+local actions = require('telescope.actions')
+local builtin = require('telescope.builtin')
+require('telescope').setup {
+    pickers = {
+        find_files = {
+            hidden = true
+        }
+    },
+      defaults = {
+        mappings = {
+          i = {
+            ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
+          },
+          n = {
+            ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
+          }
+        }
+      },
+}
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, opts)
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, opts)
+vim.keymap.set('n', '<leader>fb', builtin.buffers, opts)
+vim.keymap.set('n', '<leader>ft', builtin.treesitter, opts)
+vim.keymap.set('n', '<leader>fh', '<cmd>:Telescope todo-comments todo<CR>', opts)
+vim.keymap.set('n', '<leader>d', function() actions.delete_buffer() end)
+
